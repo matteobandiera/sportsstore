129928ce83d13cb2bfcfe7654c0c3339b3e9fd0b
matteo bandiera <misterflag86@gmail.com>
2015-08-04 15:41:59 +0100

    Bug fixed cart.js and checkoutSummary.js

--- components/cart/cart.js
+++ components/cart/cart.js
@@ -21,7 +21,7 @@ angular.module("cart", [])
 		},
 		
 		removeProduct: function(id){
-			for(var i = 0; i < cartData.lenght; i++){
+			for(var i = 0; i < cartData.length; i++){
 				if(cartData[i].id == id){
 					cartData.splice(i, 1);
 					break;
